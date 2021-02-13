a437507347b16e6644b18f7171a1e3ecdcbe098d
fernando peguero <fernandoapeguero@gmail.com>
2021-02-13 16:16:08 -0500

    added new fields to team model and event model

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from operator import truediv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import SQLALCHEMY_DATABASE_URI
@@ -14,6 +15,8 @@ class Team(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), nullable=False)
+    wins = db.Column(db.String(10), nullable=True, default=0)
+    losses = db.Column(db.String(10), nullable=True, default=0)
     home_state = db.Column(db.String(), nullable=False)
 
 
@@ -22,6 +25,7 @@ class Venue(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(500), nullable=True)
     address = db.Column(db.String(150))
     city = db.Column(db.String(50))
     zipcode = db.Column(db.String(10))
@@ -32,6 +36,8 @@ class Events(db.Model):
     __tablename__ = 'events'
     
     id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime())
+    
     team_id = db.Column(db.Integer(), db.ForeignKey('teams.id'))
     venue_id = db.Column(db.Integer(), db.ForeignKey('venues.id'))
 
