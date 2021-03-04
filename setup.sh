96418ad5cbbae4c00ec2b7cea11433e0846c9b31
fernando peguero <fernandoapeguero@gmail.com>
2021-03-04 00:55:21 -0500

    endpoint not working unknown

--- setup.sh
+++
@@ -1,3 +0,0 @@
-export AUTH_DOMAIN = 'auth0-fsnd.us.auth0.com'
-export API_AUDIENCE = 'nba_data_api'
-export DATABASE_URL  = 'postgres://ufcrnranlfnlng:648ef2192277da2edf643ac74a434089165fa2193cfd9ab4563d1cbba89fdeb1@ec2-54-146-73-98.compute-1.amazonaws.com:5432/db1cjuu2ee9u8c'
