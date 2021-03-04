d71cefec58daa762ae0c5441ad8436bdf080ac5f
fernando peguero <fernandoapeguero@gmail.com>
2021-03-03 20:51:50 -0500

    update database url

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 export API_AUDIENCE = 'nba_data_api'
 export AUTH_DOMAIN = 'auth0-fsnd.us.auth0.com'
-export DATABASE_URL  = 'postgres://tlgbgxdvfjqxgp:5cb6568fbed3ecd765418f89fc6c23e079944cc1b63d7967899af51d75d2dcdf@ec2-18-211-97-89.compute-1.amazonaws.com:5432/d734kkhh9nd09c'
+export DATABASE_URL  = 'postgres://ufcrnranlfnlng:648ef2192277da2edf643ac74a434089165fa2193cfd9ab4563d1cbba89fdeb1@ec2-54-146-73-98.compute-1.amazonaws.com:5432/db1cjuu2ee9u8c'
